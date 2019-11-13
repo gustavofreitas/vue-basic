2dd5583c09e6b6882548ea14c5b5d1c45bf0b119
Gustavo Freitas <gustavopozes.freitas@gmail.com>
2019-11-12 22:26:56 -0300

    final version

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{todo.text}}</li>'
+})
+
 var app = new Vue({ 
     el: '#app',
     data: {
-        message: 'Hello Gustavo!'
+        message: 'Hello Vue!',
+        list: [
+            {id: 0, text: 'tomate'},
+            {id: 1, text: "laranja"},
+            {id: 2, text: "ovos"},
+            {id: 3, text: "my ovos"},
+            {id: 4, text: "mi cohones"},
+            {id: 5, text: "meus eggs"}
+
+        ]
     }
 });
